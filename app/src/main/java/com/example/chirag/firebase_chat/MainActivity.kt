1e449f7203de2347af0694f6369f350e99bd21ad
chirag <chirag@entigrity.com>
2019-08-09 20:27:30 +0530

    firebase_cloud_storage

---
+++ app/src/main/java/com/example/chirag/firebase_chat/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.chirag.firebase_chat
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
